4e7b49d14e66b7d429b9627d17a5a6735bdc6468
TheCoder1961 <83503355+TheCoder1961@users.noreply.github.com>
2021-07-16 17:36:08 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    execute(message, args) {
-        message.channel.send('Pong.');
-    },
+	name: 'ping',
+	description: 'Ping!',
+    usage: 'usage here',
+	execute(message) {
+		message.channel.send('Pong.');
+	},
 };
