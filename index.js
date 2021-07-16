106aa341728ba106905283d8e22f6a4ccf9a780c
Rismose <rasmus.h.e@hotmail.se>
2021-07-16 12:55:01 +0200

    stuff

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 require('dotenv').config()
 const Discord = require('discord.js')
-const client = new Discord.Client()
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-});
+const testGuild = '813034684614311997'
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_EMOJIS",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGES",
+      ],
+})
 
-client.on('message', msg => {
-    if(msg.content == 'ping') {
-        msg.reply('pong');
+const getApp = (testGuild) => {
+    const app = client.api.applications(client.user.id)
+    if (testGuild) {
+        app.guilds(testGuild)
     }
-});
+    return app
+}
 
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+
+    const commands = await getApp(testGuild).commands.get()
+console.log(commands)
+});
 
 client.login(process.env.token);
