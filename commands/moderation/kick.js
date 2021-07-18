e7b841bc8eaa1e66a814666e2793a4de81dcfdbd
TheCoder1961 <83503355+TheCoder1961@users.noreply.github.com>
2021-07-18 12:06:31 +0530

    Update kick.js

---
+++ commands/moderation/kick.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const fs = require('fs');
+
+module.exports = {
+	name: 'botinfo',
+	description: 'View bot info.',
+	commandOptions: null,
+    global: false,
+	execute(interaction) {
+const args1 = interaction.data.options[0].value.split(" ")
+const reason = args1
+const args2 = interaction.data.options[1].mentions.users.first()
+await args2.kick().then(mem => {
+message.channel.send(`kicked ${mem.username}`)
+return
+})
+}
+}
