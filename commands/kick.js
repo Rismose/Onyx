1d992440718c5bfe023d19cba64c8960c7097528
Rismose <rasmus.h.e@hotmail.se>
2021-07-18 19:10:26 +0200

    working purge/kick due to lemons <3

--- commands/kick.js
+++ commands/kick.js
@@ -3,14 +3,13 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'kick',
     description: 'bots kick command!',
-	commandOptions: [
-        {
-            type: 3,
+    commandOptions: [{
+            type: 6,
             name: "user-tag",
             description: "User to kick!",
             required: true
         },
-{
+        {
             type: 3,
             name: "reason",
             description: "why kick user!",
@@ -19,17 +18,19 @@ module.exports = {
     ],
     global: false,
     execute(interaction) {
-    const user = interaction.data.options[1].mentions.users.first()
-    const reason = interaction.data.options[0].value.split(" ")
-    user.kick().then(mem => {
-
-    client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-            type: 4,
-            data: {
-                    content: `kicked ${mem.username} for ${reason}`
-                }
-            }
+        const user = interaction.data.options[0].value
+        const reason = interaction.data.options[1].value
+        client.guilds.fetch(interaction.guild_id).then(guild => {
+            guild.members.kick(user, reason).then(user => {
+                client.api.interactions(interaction.id, interaction.token).callback.post({
+                    data: {
+                        type: 4,
+                        data: {
+                            content: `Successfully kicked **${user.username}** for ${reason}`
+                        }
+                    }
+                })
+            })
         })
-})
-}
+    }
 }
--- commands/purge.js
+++ commands/purge.js
@@ -3,55 +3,52 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'purge',
     description: 'bots purge command!',
-	commandOptions: [
-        {
-            type: 3,
-            name: "purge-amount",
-            description: "How many messages to purge!",
-            required: true
-        }
-    ],
+    commandOptions: [{
+        type: 4,
+        name: "purge-amount",
+        description: "How many messages to purge!",
+        required: true
+    }],
     global: false,
-    execute(interaction) {
-    const channel = client.channels.cache.get(interaction.channel_id),
-    amount = interaction.data.options[0].value;
+    async execute(interaction) {
+        const channel = await client.channels.fetch(interaction.channel_id),
+            amount = interaction.data.options[0].value;
 
-    channel.bulkDelete(amount)
+        if (isNaN(amount)) {
+            const resultembedn = new Discord.MessageEmbed()
+                .setColor("#3371FF")
+                .setTitle("Invalid.")
+                .setDescription("Invalid Arguments.")
 
-    function logResults(error, results) {
-        if (error) {
-            console.log(error);
-            message.channel.send("An error occurred.")
-        } else {
-
-            if(!results[0]) {
-                const resultembedn = new Discord.MessageEmbed()
-                    .setColor("#3371FF")
-                    .setTitle("Invalid.")
-                    .setDescription("Invalid Arguments.")
-
-                return client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+            return client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
                     type: 4,
                     data: {
-                            embeds: [resultembedn]
-                        }
-                    }
-                })
-            }
-
-            const resultembed = new Discord.MessageEmbed()
-                .setColor("#3371FF")
-                .setTitle(`Purged Successfully - ${search}`)
-                .setFooter(amount + " messages purged")
-
-            client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-                type: 4,
-                data: {
-                        embeds: [resultembed]
+                        embeds: [resultembedn]
                     }
                 }
             })
         }
-    }
+
+        channel.bulkDelete(amount)
+            .then(() => {
+                const resultembed = new Discord.MessageEmbed()
+                    .setColor("#3371FF")
+                    .setTitle(`Purged Successfully`)
+                    .setFooter(amount + " messages purged")
+
+                client.api.interactions(interaction.id, interaction.token).callback.post({
+                    data: {
+                        type: 4,
+                        data: {
+                            embeds: [resultembed]
+                        }
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                channel.send("An error occurred.")
+            })
     }
 }
