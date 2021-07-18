2f70376445949a66706f2465634a0e665d042c95
TheCoder1961 <83503355+TheCoder1961@users.noreply.github.com>
2021-07-18 12:08:46 +0530

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -4,12 +4,25 @@ const fs = require('fs');
 module.exports = {
 	name: 'kick',
 	description: 'bots kick command!',
-	commandOptions: null,
+	commandOptions: [
+		{
+			type: 3,
+            name: "user",
+            description: "user to kick!",
+            required: true
+		}
+    ],
+[
+type: 3,
+            name: "reason",
+            description: "why did u kick this user!!",
+            required: true
+],
     global: false,
 	execute(interaction) {
-const args1 = interaction.data.options[0].value.split(" ")
+const args1 = interaction.data.options[1].value.split(" ")
 const reason = args1
-const args2 = interaction.data.options[1].mentions.users.first()
+const args2 = interaction.data.options[0].mentions.users.first()
 await args2.kick().then(mem => {
 message.channel.send(`kicked ${mem.username}`)
 return
